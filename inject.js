22f546559ed208d170100504c878f9801c921522
idk-v1 <104273594+idk-v1@users.noreply.github.com>
2025-01-04 12:13:52 -0600

    kinda revert pt1

--- inject.js
+++ inject.js
@@ -306,12 +306,11 @@ function defineVideoController() {
     }">
           <span data-action="drag" class="draggable">${speed}</span>
           <span id="controls">
+            <button data-action="rewind" class="rw">«</button>
             <button data-action="slower">&minus;</button>
             <button data-action="faster">&plus;</button>
+            <button data-action="advance" class="rw">»</button>
             <button data-action="display" class="hideButton">&times;</button>
-            
-            <button data-action="rewind" class="rw,left">«</button>
-            <button data-action="advance" class="rw,right">»</button>
           </span>
         </div>
       `;
