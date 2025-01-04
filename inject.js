d07f1cd38ac31dddbfc91486c728ba004aee258b
idk-v1 <104273594+idk-v1@users.noreply.github.com>
2025-01-04 12:34:20 -0600

    whoops changed wrong element

--- inject.js
+++ inject.js
@@ -308,12 +308,12 @@ function defineVideoController() {
       tc.settings.controllerOpacity
     }">
           <span data-action="drag" class="draggable">${speed}</span>
-          <span id="controls" style="border: ${tc.settings.controllerScale}px solid white;">
-            <button data-action="rewind" class="rw">«</button>
-            <button data-action="slower">&minus;</button>
-            <button data-action="faster">&plus;</button>
-            <button data-action="advance" class="rw">»</button>
-            <button data-action="display" class="hideButton">&times;</button>
+          <span id="controls">
+            <button data-action="rewind" class="rw" style="border: ${tc.settings.controllerScale}px solid white;">«</button>
+            <button data-action="slower" style="border: ${tc.settings.controllerScale}px solid white;">&minus;</button>
+            <button data-action="faster" style="border: ${tc.settings.controllerScale}px solid white;">&plus;</button>
+            <button data-action="advance" class="rw" style="border: ${tc.settings.controllerScale}px solid white;">»</button>
+            <button data-action="display" class="hideButton" style="border: ${tc.settings.controllerScale}px solid white;">&times;</button>
           </span>
         </div>
       `;
